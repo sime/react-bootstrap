55f1b882878453d79e7c0a5db20561ff3013891b
Simon Males <sime@sime.net.au>
2020-10-23 19:39:46 +0200

    Bojagify with Alert component

---
+++ .bojagirc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  webpackConfig: 'tools/dist.webpack.config.js',
+}
---
+++ src/Alert.bojagi.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Alert from './Alert';
+
+export const alerts = () => [
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+  'light',
+  'dark',
+].map((variant, idx) => (
+  <Alert key={idx} variant={variant}>
+    This is a {variant} alert—check it out!
+  </Alert>
+));
